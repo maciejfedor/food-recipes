e66d4cee49421254f2d3bf28ed7cbc7bc93e4c3b
Maciej Fedor <fedor.maciej@gmail.com>
2022-07-30 20:02:01 +0200

    add pagination feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -33,14 +34,22 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     resultsView.render(model.getSearchResultsPage());
+
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandleClick(controlPagination);
 };
 
 init();
